08f22779ad494ed13abff294ac182d7d956b98d9
vjumpkung <vjumpkung@gmail.com>
2023-01-26 21:03:08 +0700

    update shortest path solution

--- week5/shortestpath.cpp
+++ week5/shortestpath.cpp
@@ -1,40 +1,10 @@
 #include <iostream>
-#include <queue>
+#include <set>
 #include <utility>
 #include <vector>
 #define INF 0xfffffff
 
 using namespace std;
-
-int shortest_path(vector<vector<pair<int, int>>> &Graph, int source)
-{
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    vector<int> distance(Graph.size(), INF);
-    pq.push({0, source});
-    distance[source] = 0;
-    vector<bool> f(Graph.size(), false);
-    while (not pq.empty())
-    {
-        int u = pq.top().second;
-        pq.pop();
-        if (f[u])
-        {
-            continue;
-        }
-        f[u] = true;
-        for (auto &i : Graph.at(u))
-        {
-            int v = i.second;
-            int weight = i.first;
-            if (f[v] == false and distance[v] > distance[u] + weight)
-            {
-                distance[v] = distance[u] + weight;
-                pq.push({distance[v], v});
-            }
-        }
-    }
-    return distance[Graph.size() - 1];
-}
 
 int main()
 {
@@ -49,6 +19,31 @@ int main()
         graph.at(from - 1).push_back({weight, too - 1});
         graph.at(too - 1).push_back({weight, from - 1});
     }
-    cout << shortest_path(graph, 0) << endl;
+    set<pair<int, int>> pq;
+    vector<int> distance(graph.size(), INF);
+    vector<bool> visited(graph.size(), false);
+    pq.insert({0, 0});
+    distance.at(0) = 0;
+    while (!pq.empty())
+    {
+        int u = (*pq.begin()).second;
+        pq.erase(pq.begin());
+        if (visited[u])
+        {
+            continue;
+        }
+        visited[u] = true;
+        for (auto i : graph.at(u))
+        {
+            int v = i.second;
+            int weight = i.first;
+            if (visited[v] == false and distance[v] > distance[u] + weight)
+            {
+                distance[v] = distance[u] + weight;
+                pq.insert({distance[v], v});
+            }
+        }
+    }
+    cout << distance[graph.size() - 1] << endl;
     return 0;
 }
