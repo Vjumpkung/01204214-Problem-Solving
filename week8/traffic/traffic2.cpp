cbdc1e5daf4a47e0a294dfbc0179b406097c1b8b
vjumpkung <vjumpkung@gmail.com>
2023-02-26 20:15:57 +0700

    update traffic2

--- week8/traffic/traffic2.cpp
+++ week8/traffic/traffic2.cpp
@@ -1,118 +1,115 @@
 #include "traffic.h"
 #include <cmath>
 #include <cstdio>
+#include <vector>
+
+int n, k;
+int vertical_detect = 0;
+int horizontal_detect = 0;
+int answer[2][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}};
+int low, high, mid, total, ansCount = 0, temp;
+
+using namespace std;
+
+void vertical(int i, int low, int high, int total)
+{
+    int mid = floor(abs(high + low) / 2);
+    if (abs(low - high) == 1 and total == 2)
+    {
+        for (int j = 0; j < ansCount; j++)
+        {
+            if (answer[j][0] == i and answer[j][1] == low and answer[j][2] == i and answer[j][3] == high)
+            {
+                return;
+            }
+        }
+        answer[ansCount][0] = i;
+        answer[ansCount][1] = low;
+        answer[ansCount][2] = i;
+        answer[ansCount][3] = high;
+        ansCount++;
+    }
+    else if (total <= abs(low - high))
+    {
+        return;
+    }
+    else
+    {
+        int temp = traffic_query(i, low, i, mid);
+        if (temp > abs(low - mid))
+        {
+            vertical(i, low, mid, temp);
+        }
+        if (total - temp > abs(mid - high))
+        {
+            vertical(i, mid, high, total - temp);
+        }
+    }
+}
+
+void horizontal(int i, int low, int high, int total)
+{
+    mid = floor(abs(high + low) / 2);
+    if (abs(low - high) == 1 and total == 2)
+    {
+        for (int j = 0; j < ansCount; j++)
+        {
+            if (answer[j][0] == low and answer[j][1] == i and answer[j][2] == high and answer[j][3] == i)
+            {
+                return;
+            }
+        }
+        answer[ansCount][0] = low;
+        answer[ansCount][1] = i;
+        answer[ansCount][2] = high;
+        answer[ansCount][3] = i;
+        ansCount++;
+    }
+    else if (total <= abs(low - high))
+    {
+        return;
+    }
+    else
+    {
+        temp = traffic_query(low, i, mid, i);
+        if (temp > abs(low - mid))
+        {
+            horizontal(i, low, mid, temp);
+        }
+        if (total - temp > abs(mid - high))
+        {
+            horizontal(i, mid, high, total - temp);
+        }
+    }
+}
 
 int main()
 {
-    int n, k;
     traffic_init(&n, &k);
-    int x1, x2, y1, y2;
-    int vertical_detect = 0;
-    int horizontal_detect = 0;
-    x1 = 1;
-    x2 = n;
-    y1 = 1;
-    y2 = n;
     // vertical line
     for (int i = 1; i <= n; i++)
     {
-        if (traffic_query(i, 1, i, n) > n - 1)
+        total = traffic_query(i, 1, i, n);
+        low = 1;
+        high = n;
+        if (total > n - 1)
         {
-            vertical_detect = 1;
-            x1 = i;
-            x2 = i;
-            break;
+            vertical(i, low, high, total);
         }
     }
     // horizontal line
     for (int i = 1; i <= n; i++)
     {
-        if (traffic_query(1, i, n, i) > n - 1)
+        total = traffic_query(1, i, n, i);
+        low = 1;
+        high = n;
+        if (total > n - 1)
         {
-            horizontal_detect = 1;
-            y1 = i;
-            y2 = i;
-            break;
+            horizontal(i, low, high, total);
         }
     }
-    int ans1x1 = 0, ans1y1 = 0, ans1x2 = 0, ans1y2 = 0;
-    int ans2x1 = 0, ans2y1 = 0, ans2x2 = 0, ans2y2 = 0;
-    if (vertical_detect)
-    {
-        int low = n;
-        int high = 1;
-        while (1)
-        {
-            int mid = (low + high) / 2;
-            if (traffic_query(x1, mid - 1, x1, mid) == 2)
-            {
-                ans1x1 = x1;
-                ans1y1 = mid - 1;
-                ans1x2 = x1;
-                ans1y2 = mid;
-                break;
-            }
-            else if (traffic_query(x1, mid, x1, mid + 1) == 2)
-            {
-                ans1x1 = x1;
-                ans1y1 = mid;
-                ans1x2 = x1;
-                ans1y2 = mid + 1;
-                break;
-            }
-            else if (traffic_query(x1, low, x1, mid) > abs(low - mid))
-            {
-                high = mid;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-    }
-    if (horizontal_detect)
-    {
-        int low = n;
-        int high = 1;
-        while (1)
-        {
-            int mid = (low + high) / 2;
-            if (traffic_query(mid - 1, y1, mid, y1) == 2)
-            {
-                ans2x1 = mid - 1;
-                ans2y1 = y1;
-                ans2x2 = mid;
-                ans2y2 = y1;
-                break;
-            }
-            else if (traffic_query(mid, y1, mid + 1, y1) == 2)
-            {
-                ans2x1 = mid;
-                ans2y1 = y1;
-                ans2x2 = mid + 1;
-                ans2y2 = y1;
-                break;
-            }
-            else if (traffic_query(low, y1, mid, y1) > abs(low - mid))
-            {
-                high = mid;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-    }
-    if (k == 1)
-    {
-        if (vertical_detect)
-            traffic_report(ans1x1, ans1y1, ans1x2, ans1y2, 0, 0, 0, 0);
-        else if (horizontal_detect)
-            traffic_report(ans2x1, ans2y1, ans2x2, ans2y2, 0, 0, 0, 0);
-    }
-    else
-    {
-        traffic_report(ans1x1, ans1y1, ans1x2, ans1y2, ans2x1, ans2y1, ans2x2, ans2y2);
-    }
+
+    traffic_report(answer[0][0], answer[0][1], answer[0][2], answer[0][3], answer[1][0], answer[1][1], answer[1][2], answer[1][3]);
+
     return 0;
 }
