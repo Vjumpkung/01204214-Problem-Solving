f365d3dbf38d56eda0f5bccdc1a0a996b88d825c
vjumpkung <vjumpkung@gmail.com>
2023-03-10 00:01:44 +0700

    add optional score

--- week9/fin_star.cpp
+++ week9/fin_star.cpp
@@ -3,47 +3,42 @@
 using namespace std;
 
 int n;
-int p[100001][2];
-int storage[100001][2];
+int p[100001][3];
+int dp[100001][3];
 
 /*
-mode 0 = coffee
-mode 1 = bakery
+mode 0 = any
+mode 1 = coffee
+mode 2 = bakery
 */
 
 int star(int idx, int mode)
 {
-    if (storage[idx][mode] != -1)
+    if (dp[idx][mode] != -1)
     {
-        return storage[idx][mode];
+        return dp[idx][mode];
     }
-    if (idx > n - 1)
+    if (idx >= n)
     {
         return 0;
     }
     else
     {
-        int a, b, c, d, e;
-        int maxx = -10001;
         if (mode == 0)
         {
-            a = star(idx + 1, 1);
+            dp[idx][mode] = max(star(idx + 1, 0), max(star(idx + 1, 1), star(idx + 1, 2)));
+            return dp[idx][mode];
+        }
+        else if (mode == 1)
+        {
+            dp[idx][mode] = p[idx][1] + max(star(idx + 1, 2), star(idx + 1, 0));
+            return dp[idx][mode];
         }
         else
         {
-            a = star(idx + 1, 0);
+            dp[idx][mode] = p[idx][2] + max(star(idx + 1, 1), star(idx + 1, 0));
+            return dp[idx][mode];
         }
-        maxx = max(maxx, a);
-        b = star(idx + 2, 0);
-        maxx = max(maxx, b);
-        c = star(idx + 2, 1);
-        maxx = max(maxx, c);
-        d = star(idx + 3, 0);
-        maxx = max(maxx, d);
-        e = star(idx + 3, 1);
-        maxx = max(maxx, e);
-        storage[idx][mode] = maxx + p[idx][mode];
-        return storage[idx][mode];
     }
     return 0;
 }
@@ -53,17 +48,17 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        cin >> p[i][0] >> p[i][1];
-        storage[i][0] = -1;
-        storage[i][1] = -1;
+        cin >> p[i][1] >> p[i][2];
+        dp[i][0] = -1;
+        dp[i][1] = -1;
+        dp[i][2] = -1;
     }
-    int ans2 = max(star(0, 0), star(0, 1));
-    int ans1 = max(star(1, 0), star(1, 1));
-    if (ans2 < 0 and ans1 < 0)
+    int ans1 = max(star(0, 0), max(star(0, 1), star(0, 2)));
+    if (ans1 < 0)
     {
         cout << 0 << "\n";
         return 0;
     }
-    cout << max(ans2, ans1) << "\n";
+    cout << ans1 << "\n";
     return 0;
 }
