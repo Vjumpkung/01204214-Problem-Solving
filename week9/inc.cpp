f021178421e14e86579d9e6bad96b41a6aa5217a
vjumpkung <vjumpkung@gmail.com>
2023-03-10 01:39:23 +0700

    change inc solution to shorter one

--- week9/inc.cpp
+++ week9/inc.cpp
@@ -3,79 +3,50 @@
 
 using namespace std;
 
-int n;
-vector<int> sub;
-vector<int> dp;
-
-int lengthOfLIS(vector<int> &nums)
-{
-    // with given sequence
-    int n = nums.size();
-    for (int i = 0; i < n; i++)
-    {
-        sub.push_back(-1);
-        dp.push_back(1);
-    }
-    if (n == 0)
-        return 0;
-    int ans = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (nums[i] > nums[j])
-            {
-                // cout << i << " " << j << "\n";
-                if (dp[i] < dp[j] + 1)
-                {
-                    sub[i] = j;
-                }
-                dp[i] = max(dp[i], dp[j] + 1);
-                ans = max(ans, dp[i]);
-            }
-        }
-    }
-    return ans;
-}
+int n, ans = 1;
+int a[1000];
+int dp[1000];
+vector<int> v[1000];
 
 int main()
 {
     cin >> n;
-    vector<int> arr(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        dp[i] = 1;
+        cin >> a[i];
     }
-    cout << lengthOfLIS(arr) << endl;
-    // print dp
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << dp[i] << " ";
-    // }
-    // get max of dp with index
-    int maxx = 0;
     for (int i = 0; i < n; i++)
     {
-        if (dp[i] > dp[maxx])
+        for (int j = 0; j < i; j++)
         {
-            maxx = i;
+            if (a[i] > a[j])
+            {
+                if (dp[i] < dp[j] + 1)
+                {
+                    dp[i] = dp[j] + 1;
+                    v[i].push_back(a[j]);
+                }
+                if (ans < dp[i])
+                {
+                    ans = dp[i];
+                }
+            }
+        }
+        v[i].push_back(a[i]);
+    }
+    cout << ans << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        if (dp[i] == ans)
+        {
+            for (int j = 0; j < v[i].size(); j++)
+            {
+                cout << v[i][j] << " ";
+            }
+            cout << "\n";
+            break;
         }
     }
-    // cout << "\n";
-    vector<int> res;
-    int j = maxx;
-    res.push_back(j);
-    while (sub[j] != -1)
-    {
-        // cout << sub[j] << " ";
-        j = sub[j];
-        res.push_back(j);
-    }
-    // cout << "\n";
-    for (int i = res.size() - 1; i >= 0; i--)
-    {
-        cout << arr[res[i]] << " ";
-    }
-    cout << "\n";
     return 0;
 }
