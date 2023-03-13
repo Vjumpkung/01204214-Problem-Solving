f664e9b52efbe677631ceec57e1237e03c4370e9
vjumpkung <vjumpkung@gmail.com>
2023-03-13 12:43:54 +0700

    update seqalign

---
+++ week10/lcs1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);
+
+int dp[201][201];
+
+int main()
+{
+    fastio;
+    string s,t;
+    cin >> s >> t;
+    return 0;
+}
